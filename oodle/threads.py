de709302535107851138c9194fb821db0031540e
Zech Zimmerman <hi@zech.codes>
2024-11-29 04:01:03 -0500

    Implement thread interruption with shields

--- oodle/threads.py
+++ oodle/threads.py
@@ -1,6 +1,7 @@
+import ctypes
 from collections.abc import Callable
-from sys import setprofile
-from threading import Thread as _Thread, Event
+from threading import Thread as _Thread, Event, Lock
+from typing import Any
 
 
 class ExitThread(Exception):
@@ -11,14 +12,20 @@ class InterruptibleThread(_Thread):
     def __init__(
         self,
         *args,
-        stop_callback: Callable[[], None] | None = None,
         cancel_callback: Callable[[], None] | None = None,
+        stop_callback: Callable[[], None] | None = None,
         **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self._shield_lock = Lock()
         self._cancel_callback = cancel_callback
+        self._pending_stop_event = Event()
+        self._shield_lock = Lock()
         self._stop_callback = stop_callback
+
+    @property
+    def pending_stop_event(self) -> Event:
+        return self._pending_stop_event
+
     @property
     def shield(self) -> Lock:
         return self._shield_lock
@@ -27,18 +34,46 @@ class InterruptibleThread(_Thread):
         try:
             super().run()
         except Exception as e:
-            if self._cancel_callback:
-                self._cancel_callback()
-
+            self._run_callback(self._cancel_callback)
             if not isinstance(e, ExitThread):
                 raise
         else:
-            if self._stop_callback:
-                self._stop_callback()
+            self._run_callback(self._stop_callback)
 
     def stop(self, timeout: float = 0):
+        if not self.shield.locked():
+            self._pending_stop_event.set()
 
+        counter = 0
+        fractional_timeout = timeout / 100 if timeout > 0 else None
+        while self.is_alive():
+            if self.shield.locked():
+                if not self.shield.acquire(timeout=fractional_timeout):
+                    break
 
+                self.pending_stop_event.set()
+            else:
+                self.throw(ExitThread())
+                self.join(fractional_timeout)
+
+            counter += 1
+            if counter > 100 and timeout > 0:
+                break
+
+        else:
+            return
+
+        raise TimeoutError("Failed to stop thread")
+
+    def throw(self, exception: Exception):
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(
+            ctypes.c_long(self.ident),
+            ctypes.py_object(exception),
+        )
+
+    def _run_callback(self, callback: Callable[[], None] | None):
+        if callback is not None:
+            callback()
 
 
 class Thread:
@@ -60,13 +95,20 @@ class Thread:
         self._thread.join(timeout)
 
     @classmethod
-    def spawn(cls, target, args, kwargs, stop_callback: Callable[[], None] | None=None, cancel_callback: Callable[[], None] | None=None):
+    def spawn(
+        cls,
+        target: Callable[[Any, ...], Any],
+        args: tuple[Any, ...],
+        kwargs: dict[str, Any],
+        stop_callback: Callable[[], None] | None=None,
+        cancel_callback: Callable[[], None] | None=None
+    ):
         thread = InterruptibleThread(
             target=target,
             args=args,
             kwargs=kwargs,
-            stop_callback=stop_callback,
             cancel_callback=cancel_callback,
+            stop_callback=stop_callback,
             daemon=True
         )
         thread.start()
