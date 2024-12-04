29b2d637cec3dc0f5f51b1b35e74de86fedca374
Zech Zimmerman <hi@zech.codes>
2024-12-04 08:58:42 -0500

    Implement new group shutdown process

--- oodle/thread_groups.py
+++ oodle/thread_groups.py
@@ -1,6 +1,14 @@
-from threading import Event, Semaphore
+from dataclasses import dataclass
+from threading import Event, Semaphore, Thread
 
-from .spawners import spawn, Spawner
+from .mutex import Mutex
+from .spawners import Spawner
+
+
+@dataclass
+class ThreadExceptionInfo:
+    thread: Thread
+    exception: Exception
 
 
 class ThreadGroup:
@@ -9,6 +17,7 @@ class ThreadGroup:
         self._stop_event = Event()
         self._exception_mutex = Mutex()
         self._exception: ThreadExceptionInfo | None = None
+        self._shutdown_event = Event()
 
         self._spawner = Spawner(self._build_thread)
 
@@ -25,8 +34,11 @@ class ThreadGroup:
         self._shutdown_event.set()
         self._stop_event.set()
 
+    def _stop_threads(self):
+        for thread in self._threads:
+            if thread.is_alive:
+                thread.stop()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
     def thread_encountered_exception(self, thread: Thread, exception):
         if self._stop_event.is_set():
             return
@@ -39,13 +51,21 @@ class ThreadGroup:
     def thread_stopped(self, thread: Thread):
         self._stop_event.set()
 
+    def wait(self):
         while any(thread.is_alive for thread in self._threads):
             self._stop_event.wait()
+
+            if self._shutdown_event.is_set():
+                self._stop_threads()
+
+                if self._exception:
+                    raise self._exception.exception
+
             self._stop_event.clear()
 
-            if self._cancel_event.is_set():
-                break
     def __enter__(self):
         return self
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.wait()
         return
