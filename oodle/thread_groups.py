cda6fe9f76f8a54d93387626e80f2020fb19569c
Zech Zimmerman <hi@zech.codes>
2024-12-04 08:55:51 -0500

    Change semantics of group spawning

--- oodle/thread_groups.py
+++ oodle/thread_groups.py
@@ -9,13 +9,17 @@ class ThreadGroup:
         self._cancel_event = Event()
         self._stop_event = Event()
 
+        self._spawner = Spawner(self._build_thread)
+
+    @property
+    def spawn(self) -> Spawner:
+        return self._spawner
+
     def _build_thread(self, func, *args, **kwargs):
         thread = Spawner(stop_callback=self._stop_event.set, cancel_callback=self._cancel_event.set)[func](*args, **kwargs)
         self._threads.append(thread)
         return thread
 
-    def __enter__(self):
-        return Spawner(self._build_thread)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         while any(thread.is_alive for thread in self._threads):
@@ -24,5 +28,7 @@ class ThreadGroup:
 
             if self._cancel_event.is_set():
                 break
+    def __enter__(self):
+        return self
 
         return
