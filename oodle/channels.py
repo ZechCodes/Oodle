cc8b60316dd8b61ec4b662ae481d61294bfb6e19
Zech Zimmerman <hi@zech.codes>
2024-12-04 08:46:37 -0500

    Make channels closable

--- oodle/channels.py
+++ oodle/channels.py
@@ -1,9 +1,17 @@
 from queue import Queue
+from typing import Callable
 
 
 class Channel:
     def __init__(self):
         self._queue = Queue()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return False
 
     def __iter__(self):
         return self
@@ -15,11 +23,26 @@ class Channel:
         return self.get()
 
     @property
-    def is_empty(self):
+    def is_closed(self) -> bool:
+        return self._queue is None
+
+    @property
+    def is_empty(self) -> bool:
         return self._queue.empty()
 
     def put(self, value):
+    def close(self):
+        self._queue.shutdown(True)
+        self._queue = None
+
+    def put(self, value: T):
+        if self._queue is None:
+            raise ValueError("Channel is closed")
+
         self._queue.put(value)
 
     def get(self):
+        if self._queue is None:
+            raise ValueError("Channel is closed")
+
         return self._queue.get()
