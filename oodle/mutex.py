f1f645f3d56eb1a7438dd46b6139cdc8c5d27780
Zech Zimmerman <hi@zech.codes>
2024-12-05 18:49:34 -0500

    Improve the mutex

--- oodle/mutex.py
+++ oodle/mutex.py
@@ -4,19 +4,10 @@ from threading import Semaphore
 class Mutex(Semaphore):
     def __init__(self):
         super().__init__(1)
-        self._held = False
 
     def __repr__(self):
         return f"{self.__class__.__name__}(held={self.is_held})"
 
     @property
     def is_held(self) -> bool:
-        return self._held
-
-    def acquire(self, blocking = True, timeout = None):
-        super().acquire(blocking, timeout)
-        self._held = True
-
-    def release(self, n = 1):
-        super().release(n)
-        self._held = False
+        return self._value == 0
