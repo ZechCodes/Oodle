c43e7827eee8229d8660caf3d36a4510bab09926
Zech Zimmerman <hi@zech.codes>
2024-12-05 16:27:42 -0500

    Add the ability to check if a mutex is held

--- oodle/mutex.py
+++ oodle/mutex.py
@@ -4,6 +4,19 @@ from threading import Semaphore
 class Mutex(Semaphore):
     def __init__(self):
         super().__init__(1)
+        self._held = False
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(held={self._value == 1})"
+        return f"{self.__class__.__name__}(held={self.is_held})"
+
+    @property
+    def is_held(self) -> bool:
+        return self._held
+
+    def acquire(self, blocking = True, timeout = None):
+        super().acquire(blocking, timeout)
+        self._held = True
+
+    def release(self, n = 1):
+        super().release(n)
+        self._held = False
