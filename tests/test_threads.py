4985e44e882237d705b6ea3a1afc5ac96ca8d90e
Zech Zimmerman <hi@zech.codes>
2024-12-09 20:18:24 -0500

    Add tests for stopping threads with and without shields

--- tests/test_threads.py
+++ tests/test_threads.py
@@ -1,6 +1,9 @@
 from threading import Event
 
+import oodle
+from oodle.shields import Shield
 from oodle.threads import Thread
+from oodle.utilities import sleep
 
 
 def test_thread_run():
@@ -27,3 +30,31 @@ def test_stop():
     e.set()
     t.wait()
     assert v == 0
+
+
+def test_stop_sleep():
+    v = 0
+    def f():
+        nonlocal v
+        sleep(100)
+        v = 1
+
+    t = Thread.run(f)
+    t.stop(wait=True)
+    assert v == 0
+
+
+def test_stop_sleep_with_shield():
+    v = 0
+    e = Event()
+    def f():
+        nonlocal v
+        with Shield():
+            e.set()
+            sleep(0.01)
+            v = 1
+
+    t = Thread.run(f)
+    e.wait()
+    t.stop(wait=True)
+    assert v == 1
